be3c243c1ac3e60ca3b9a58f515983bf28242d8e
githubuserx <githubuserx@users.noreply.github.com>
2018-09-13 15:58:55 +0200

    Change Asus spider to use JSON api

--- firmware/spiders/asus.py
+++ firmware/spiders/asus.py
@@ -1,77 +1,83 @@
+import json
+
 from scrapy import Spider
 from scrapy.http import Request
 
 from firmware.items import FirmwareImage
 from firmware.loader import FirmwareLoader
-import urlparse
 
 
 class AsusSpider(Spider):
     name = "asus"
-    region = "en"
+    region = "global"
     allowed_domains = ["asus.com"]
-    start_urls = ["https://www.asus.com/support/"]
+    start_urls = [
+        "https://www.asus.com/support/api/product.asmx/GetPDLevel"
+        "?website=global"
+        "&type=1"
+        "&typeid=0"
+        "&productflag=0"
+    ]
 
     visited = []
 
     def parse(self, response):
-        if "cid" not in response.meta:
-            for category in response.xpath("//div[@class='product-category']//a/@l1_id").extract():
-                yield Request(
-                    url=urlparse.urljoin(response.url, "/support/utilities/GetProducts.aspx?ln=%s&p=%s" % (self.region, category)),
-                    meta={"cid": category},
-                    headers={"Referer": response.url,
-                             "X-Requested-With": "XMLHttpRequest"},
-                    callback=self.parse)
+        data = json.loads(response.body_as_unicode())
 
-        elif "sid" not in response.meta:
-            for series in response.xpath("//table/id/text()").extract():
+        if "category" not in response.meta:
+            categories = []
+
+            for _, level in data["Result"]["ProductLevel"].items():
+                categories += level["Items"]
+
+            for category in categories:
+                url = "https://www.asus.com/support/api/product.asmx/GetPDLevel" \
+                      "?website=global" \
+                      "&type=1" \
+                      "&typeid={}" \
+                      "&productflag=1"
+
                 yield Request(
-                    url=urlparse.urljoin(response.url, "/support/utilities/GetProducts.aspx?ln=%s&p=%s&s=%s" % (self.region, response.meta["cid"], series)),
-                    meta={"cid": response.meta["cid"], "sid": series},
+                    url=url.format(category["Id"]),
+                    meta={"category": category["Name"]},
                     headers={"Referer": response.url,
                              "X-Requested-With": "XMLHttpRequest"},
                     callback=self.parse)
 
         elif "product" not in response.meta:
-            for prod in response.xpath("//table"):
-                pid = prod.xpath("./l3_id/text()").extract()[0]
-                product = prod.xpath("./m_name/text()").extract()[0]
-                mid = prod.xpath("./m_id/text()").extract()[0]
+            for product in data["Result"]["Product"]:
+                url = "https://www.asus.com/support/api/product.asmx/GetPDBIOS" \
+                      "?website=global" \
+                      "&pdhashedid={}"
 
-                # choose "Others" = 8
                 yield Request(
-                    url=urlparse.urljoin(response.url, "/support/Download/%s/%s/%s/%s/%d" % (response.meta["cid"], response.meta["sid"], pid, mid, 8)),
-                    meta={"product": product},
+                    url=url.format(product["PDHashedId"]),
+                    meta={
+                        "category": response.meta["category"],
+                        "product": product["PDName"]
+                    },
                     headers={"Referer": response.url,
                              "X-Requested-With": "XMLHttpRequest"},
                     callback=self.parse_product)
 
     def parse_product(self, response):
-        # types: firmware = 20, gpl source = 30, bios = 3
-        for entry in response.xpath(
-                "//div[@id='div_type_20']/div[@id='download-os-answer-table']"):
-            item = FirmwareLoader(item=FirmwareImage(),
-                                  response=response, date_fmt=["%Y/%m/%d"])
+        data = json.loads(response.body_as_unicode())
 
-            version = FirmwareLoader.find_version_period(
-                entry.xpath("./p//text()").extract())
-            gpl = None
+        if data["Status"] != "SUCCESS":
+            return
 
-            # grab first download link (e.g. DLM instead of global or p2p)
-            href = entry.xpath("./table//tr[3]//a/@href").extract()[0]
+        for obj in data["Result"]["Obj"]:
+            if "firmware" not in obj["Name"].lower():
+                continue
 
-            # attempt to find matching source code entry
-            if version:
-                for source in response.xpath("//div[@id='div_type_30']/div[@id='download-os-answer-table']"):
-                    if version in "".join(source.xpath("./p//text()").extract()):
-                        gpl = source.xpath("./table//tr[3]//a/@href").extract()[0]
+            for entry in obj["Files"]:
+                item = FirmwareLoader(item=FirmwareImage(),
+                                      response=response, date_fmt=["%Y/%m/%d"])
 
-            item.add_value("version", version)
-            item.add_value("date", item.find_date(entry.xpath("./table//tr[2]/td[1]//text()").extract()))
-            item.add_value("description", " ".join(entry.xpath("./table//tr[1]//td[1]//text()").extract()))
-            item.add_value("url", href)
-            item.add_value("sdk", gpl)
-            item.add_value("product", response.meta["product"])
-            item.add_value("vendor", self.name)
-            yield item.load_item()
+                item.add_value("version", entry["Version"])
+                item.add_value("date", entry["ReleaseDate"])
+                item.add_value("description", entry["Description"])
+                item.add_value("url", entry["DownloadUrl"]["Global"])
+                item.add_value("product", response.meta["product"])
+                item.add_value("vendor", self.name)
+                yield item.load_item()
