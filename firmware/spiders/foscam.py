09bfa9f88590230b4ea253ccfbcf1c71e04d0d08
githubuserx <githubuserx@users.noreply.github.com>
2018-09-13 21:51:04 +0200

    Update foscam spider

--- firmware/spiders/foscam.py
+++ firmware/spiders/foscam.py
@@ -1,38 +1,45 @@
+import json
+import urlparse
+
 from scrapy import Spider
 from scrapy.http import Request
 
 from firmware.items import FirmwareImage
 from firmware.loader import FirmwareLoader
 
+
 class FoscamSpider(Spider):
     name = "foscam"
-    allowed_domains = ["foscam.com"]
+    allowed_domains = ["foscam.com", "www.foscam.com"]
     start_urls = [
-        "http://www.foscam.com/download-center/firmware-downloads.html"]
-
-    def start_requests(self):
-        for url in self.start_urls:
-            yield Request(url, cookies={'loginEmail': "@.com"}, dont_filter=True)
+        "https://www.foscam.com/downloads/firmwareajaxjson.html?count=1000"]
 
     def parse(self, response):
-        for i in range(0, len(response.xpath("//div[@id='main_right']/span[1]/p")), 7):
-            prods = response.xpath("//div[@id='main_right']/span[1]//p[%d]/text()" % (i + 2)).extract()[0].split("\r\n")
+        data = json.loads(response.body_as_unicode())
 
-            for product in [x for x in prods]:
-                item = FirmwareLoader(item=FirmwareImage(), response=response)
-                item.add_xpath("version", "//div[@id='main_right']/span[1]//p[%d]/text()" % (i + 3))
-                item.add_xpath("url", "//div[@id='main_right']/span[1]//p[%d]/a/@href" % (i + 7))
-                item.add_value("product", product)
-                item.add_value("vendor", self.name)
-                yield item.load_item()
+        for product in data["row"]:
+            url = "https://www.foscam.com/downloads/firmware_details.html?id={}"
 
-        for i in range(0, len(response.xpath("//div[@id='main_right']/span[2]/p")), 5):
-            prods = response.xpath("//div[@id='main_right']/span[2]//p[%d]/text()" % (i + 2)).extract()[0].split(",")
+            yield Request(
+                url=url.format(product["pid"]),
+                meta={"product": product["productname"]},
+                headers={"Referer": "https://www.foscam.com/downloads/index.html"},
+                callback=self.parse_product
+            )
 
-            for product in [x for x in prods]:
-                item = FirmwareLoader(item=FirmwareImage(), response=response)
-                item.add_xpath("version", "//div[@id='main_right']/span[2]//p[%d]/text()" % (i + 3))
-                item.add_xpath("url", "//div[@id='main_right']/span[2]//p[%d]/a/@href" % (i + 5))
-                item.add_value("product", product)
-                item.add_value("vendor", self.name)
-                yield item.load_item()
+    def parse_product(self, response):
+        table = response.xpath("//table[@class='down_table']//tr")[1:]
+
+        for row in table:
+            item = FirmwareLoader(item=FirmwareImage(), response=response)
+
+            item.add_value("version", row.xpath("td[1]/text()").extract_first())
+            item.add_value("date", row.xpath("td[2]/text()").extract_first())
+            item.add_value("description", row.xpath("td[4]/text()").extract_first())
+
+            # FIXME: download url contains '.html' which is filtered out by the pipeline
+            item.add_value("url", urlparse.urljoin("https://www.foscam.com/",
+                                                   row.xpath("td[6]/a/@href").extract_first()))
+            item.add_value("product", response.meta["product"])
+            item.add_value("vendor", self.name)
+            yield item.load_item()
