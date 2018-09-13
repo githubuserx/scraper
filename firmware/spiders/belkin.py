714b3b3180f25d388e40228b5eee58d956c9a5b0
githubuserx <githubuserx@users.noreply.github.com>
2018-09-13 16:03:53 +0200

    Update belkin spider

--- firmware/spiders/belkin.py
+++ firmware/spiders/belkin.py
@@ -1,5 +1,5 @@
 from scrapy import Spider
-from scrapy.http import Request, FormRequest, HtmlResponse
+from scrapy.http import Request
 
 from firmware.items import FirmwareImage
 from firmware.loader import FirmwareLoader
@@ -9,91 +9,45 @@ import urlparse
 
 class BelkinSpider(Spider):
     name = "belkin"
-    allowed_domains = ["belkin.com", "belkin.force.com"]
-    start_urls = ["http://www.belkin.com/us/support"]
+    allowed_domains = ["belkin.com", "belkin.force.com", "update.havaremoteserver.com"]
+    start_urls = ["https://www.belkin.com/us/search?text=firmware&type=support_downloads"]
 
     def parse(self, response):
-        if not response.xpath(
-                "//form[@id='productSearchForm']//input[@name='category']/@value").extract()[0]:
-            for category in response.xpath("//form[@id='productSearchForm']/div[1]//ul[@class='select-options']//a/@data-id").extract():
-                yield FormRequest.from_response(response,
-                                                formname="productSearchForm",
-                                                formdata={
-                                                    "category": category},
-                                                callback=self.parse)
-        elif not response.xpath("//form[@id='productSearchForm']//input[@name='subCategory']/@value").extract()[0]:
-            for subcategory in response.xpath("//form[@id='productSearchForm']/div[2]//ul[@class='select-options']//a/@data-id").extract():
-                yield FormRequest.from_response(response,
-                                                formname="productSearchForm",
-                                                formdata={
-                                                    "subCategory": subcategory},
-                                                callback=self.parse)
-        else:
-            for product in response.xpath("//form[@id='productSearchForm']/div[3]//ul[@class='select-options']//a/@data-id").extract():
+        for product in response.xpath("//ul[@class='support-list']/li/a"):
+            if "firmware" in product.xpath("./text()").extract_first().lower():
                 yield Request(
-                    url=urlparse.urljoin(
-                        response.url, "/us/support-product?pid=%s" % (product)),
+                    url=urlparse.urljoin("https://www.belkin.com/us/",
+                                         product.xpath("./@href").extract_first()),
                     headers={"Referer": response.url},
                     callback=self.parse_product)
 
     def parse_product(self, response):
-        for item in response.xpath("//div[@id='main-content']//a"):
-            if "firmware" in item.xpath(".//text()").extract()[0].lower():
-                yield Request(
-                    url=urlparse.urljoin(
-                        response.url, item.xpath(".//@href").extract()[0]),
-                    headers={"Referer": response.url},
-                    meta={"product": response.xpath("//p[@class='product-part-number']/text()").extract()[0].split(' ')[-1]},
-                    callback=self.parse_download)
+        article = response.xpath("//div[@class='support-article']")
+        product = article.xpath("./@data-article-title").extract_first() \
+            .replace(u"\u2013", "-") \
+            .rsplit("-", 1)[0] \
+            .strip()
 
-    def parse_download(self, response):
-        iframe = response.xpath(
-            "//div[@id='main-content']/iframe/@src").extract()
+        # model number
+        model = product.rsplit(",", 1)
+        if len(model) > 1:
+            product = model[1].strip()
 
-        if iframe:
-            yield Request(
-                url=iframe[0],
-                headers={"Referer": response.url},
-                meta={"product": response.meta["product"]},
-                callback=self.parse_redirect)
+        text = article.xpath(".//text()").extract()
+        href = article.xpath("./div//a/@href").extract_first()
 
-    def parse_redirect(self, response):
-        for text in response.body.split('\''):
-            if "articles/" in text.lower() and "download/" in text.lower():
-                yield Request(
-                    url=urlparse.urljoin(response.url, text),
-                    headers={"Referer": response.url},
-                    meta={"product": response.meta["product"]},
-                    callback=self.parse_kb)
+        item = FirmwareLoader(item=FirmwareImage(),
+                              response=response,
+                              date_fmt=["%b %d, %Y", "%B %d, %Y",
+                                        "%m/%d/%Y"])
 
-    def parse_kb(self, response):
-        # initial html tokenization to find regions segmented by e.g. "======"
-        # or "------"
-        filtered = response.xpath(
-            "//div[@class='sfdc_richtext']").extract()[0].split("=-")
+        version = FirmwareLoader.find_version(text)
+        if not version:
+            version = FirmwareLoader.find_version_period(text)
 
-        for entry in [x and x.strip() for x in filtered]:
-            resp = HtmlResponse(url=response.url, body=entry,
-                                encoding=response.encoding)
-
-            for link in resp.xpath("//a"):
-                href = link.xpath("@href").extract()[0]
-                if "cache-www" in href:
-                    text = resp.xpath("//text()").extract()
-                    text_next = link.xpath("following::text()").extract()
-
-                    item = FirmwareLoader(item=FirmwareImage(),
-                                          response=response,
-                                          date_fmt=["%b %d, %Y", "%B %d, %Y",
-                                                    "%m/%d/%Y"])
-
-                    version = FirmwareLoader.find_version_period(text_next)
-                    if not version:
-                        version = FirmwareLoader.find_version_period(text)
-
-                    item.add_value("version", version)
-                    item.add_value("date", item.find_date(text))
-                    item.add_value("url", href)
-                    item.add_value("product", response.meta["product"])
-                    item.add_value("vendor", self.name)
-                    yield item.load_item()
+        item.add_value("version", version)
+        item.add_value("date", item.find_date(text))
+        item.add_value("url", href)
+        item.add_value("product", product)
+        item.add_value("vendor", self.name)
+        yield item.load_item()
